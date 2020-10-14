b6d781678f63a516972b0464c8c7b322bab82ecc
Tasin Hoque <tasinhoque@gmail.com>
2020-10-14 14:16:26 +0600

    Convert server to ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,26 +1,22 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const postRoutes = require('./routes/post')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+import express from 'express'
+import mongoose from 'mongoose'
+import postRoutes from './routes/post'
+import dotenv from 'dotenv'
 
-app.use(cors())
+dotenv.config()
+const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/post', postRoutes)
-
-app.get('/', (req, res) => {
-  res.send('We are on home')
-})
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 )
 
 mongoose.connect(
-  process.env.MONGO_URI,
+  process.env.MONGO_URI || '',
   {
     useNewUrlParser: true,
     useCreateIndex: true,
