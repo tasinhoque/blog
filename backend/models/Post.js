db00b50f9edf8f95e1bde7bd63a916357f9a50ae
Tasin Hoque <tasinhoque@gmail.com>
2020-10-14 12:55:30 +0600

    Finish backend

---
+++ backend/models/Post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const postSchema = Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+module.exports = model('Post', postSchema)
